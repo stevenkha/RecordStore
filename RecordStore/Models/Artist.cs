5264d4a724e8f492e697bd97a2e9a446c76e06c0
stevenkha <stevenkha41@gmail.com>
2023-08-12 13:16:45 -0700

    created Artist model

---
+++ RecordStore/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecordStore.Models
+{
+    public class Artist
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Image { get; set; }
+        public required string Name { get; set; }
+        public string? Bio { get; set; }
+        public required List<Record> Discography { get; set; }
+    }
+}
