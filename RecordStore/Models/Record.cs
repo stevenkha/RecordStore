d1b97b5829cd3a522d2ce3f206b27413d14a1b12
stevenkha <stevenkha41@gmail.com>
2023-08-12 14:21:57 -0700

    changed DateTime to Date for Release Date

--- RecordStore/Models/Record.cs
+++ RecordStore/Models/Record.cs
@@ -9,7 +9,7 @@ namespace RecordStore.Models
         public int Id { get; set; }
         public string? Image { get; set; } 
         public required string Title { get; set; }
-        [DataType(DataType.DateTime)]
+        [DataType(DataType.Date)]
         [Display(Name = "Release Date")]
         public required DateTime ReleaseDate { get; set; } 
         public required string Artist { get; set; }
