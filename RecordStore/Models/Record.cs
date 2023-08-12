c7b73dfe8ea6e51ba0a42c55e3dd2cd2f64f01d8
stevenkha <stevenkha41@gmail.com>
2023-08-12 13:14:07 -0700

    created Record model

---
+++ RecordStore/Models/Record.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecordStore.Models
+{
+    public enum Condition
+    {
+        Poor,
+        Fair,
+        Good,
+        GoodPlus,
+        VeryGood,
+        VeryGoodPlus,
+        NearMint,
+        Mint
+    }
+
+    public class Record
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Image { get; set; } 
+        public required string Title { get; set; }
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Release Date")]
+        public required DateTime ReleaseDate { get; set; } 
+        public required string Artist { get; set; }
+        public required Condition Condition { get; set; }
+    }
+}
