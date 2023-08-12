fc31787158438ecf707b18cb83367566da00a8be
stevenkha <stevenkha41@gmail.com>
2023-08-12 13:37:40 -0700

    scaffold artist model

---
+++ RecordStore/Data/RecordStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RecordStore.Models;
+
+namespace RecordStore.Data
+{
+    public class RecordStoreContext : DbContext
+    {
+        public RecordStoreContext (DbContextOptions<RecordStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Artist> Artist { get; set; } = default!;
+    }
+}
--- RecordStore/Program.cs
+++ RecordStore/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RecordStore.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<RecordStoreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RecordStoreContext") ?? throw new InvalidOperationException("Connection string 'RecordStoreContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
