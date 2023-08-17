856a4c776feea20907004a75b2316684f269e6d8
stevenkha <stevenkha41@gmail.com>
2023-08-17 14:53:24 -0700

    created component and view model for artist dropdown and applied logic to create page for record

---
+++ RecordStore/ViewComponents/ArtistDropdownViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecordStore.Data;
+using RecordStore.ViewModels;
+
+namespace RecordStore.Views.Shared.Components
+{
+    public class ArtistDropdownViewComponent : ViewComponent
+    {
+        private readonly RecordStoreContext _context;
+
+        public ArtistDropdownViewComponent(RecordStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var artists = await _context.Artist.ToListAsync();
+            var viewModel = new ArtistDropdownViewModel
+            {
+                Artists = artists
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ RecordStore/ViewModels/ArtistDropdownViewModel.cs
@@ -0,0 +1,9 @@
+﻿using RecordStore.Models;
+
+namespace RecordStore.ViewModels
+{
+    public class ArtistDropdownViewModel
+    {
+        public required List<Artist> Artists { get; set; }
+    }
+}
