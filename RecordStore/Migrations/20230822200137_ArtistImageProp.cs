3fe70c9b759aa4882f503f73b11834a2e22de8c4
stevenkha <stevenkha41@gmail.com>
2023-08-22 13:02:01 -0700

    migration for new artist image prop

---
+++ RecordStore/Migrations/20230822200137_ArtistImageProp.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArtistImageProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Artist",
+                newName: "ImagePath");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagePath",
+                table: "Artist",
+                newName: "Image");
+        }
+    }
+}
