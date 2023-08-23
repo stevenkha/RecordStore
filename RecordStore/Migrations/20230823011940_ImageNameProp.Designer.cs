ddebbcf57897ce05e8e018be7640f5372a1e11a5
stevenkha <stevenkha41@gmail.com>
2023-08-22 18:20:06 -0700

    Image Name migration

--- RecordStore/Migrations/20230823011940_ImageNameProp.Designer.cs
+++ RecordStore/Migrations/20230823011940_ImageNameProp.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecordStore.Data;
 
@@ -11,9 +12,11 @@ using RecordStore.Data;
 namespace RecordStore.Migrations
 {
     [DbContext(typeof(RecordStoreContext))]
-    partial class RecordStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20230823011940_ImageNameProp")]
+    partial class ImageNameProp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RecordStore/Migrations/20230823011940_ImageNameProp.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageNameProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Record",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Artist",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Record");
+
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Artist");
+        }
+    }
+}
